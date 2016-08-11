d5e60b149fb8370d46e5c0c0384eadd23e1b67bd
Joshua Clark <clarkis117@live.com>
2016-08-11 11:16:40 -0400

    Finished Project

---
+++ src/MakingAFormUsingMVC/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MakingAFormUsingMVC.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MakingAFormUsingMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Person person)
+        {
+            return View(person);
+        }
+    }
+}
